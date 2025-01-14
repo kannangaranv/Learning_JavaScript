d422ae9b38cfcc627b2e43ed8a63e7bf58f44d53
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-14 12:33:57 +0530

    Arithmatic operations

---
+++ Arithmatic.js
@@ -0,0 +1,63 @@
+// Addition
+let x = 5;
+let y = 3;
+let z = x + y;
+console.log(z);
+
+// Subtraction
+let a = 5;
+let b = 3;
+let c = a - b;
+console.log(c);
+
+// division
+let d = 5;
+let e = 3;
+let f = d / e;
+console.log(f);
+
+//multiplication
+let g = 5;
+let h = 3;
+let i = g * h;  
+console.log(i);
+
+//reminder
+let j = 5;
+let k = 3;
+let l = j % k;
+console.log(l);
+
+// addition two bool
+let m = true;
+let n = false;
+let o = m + n;
+console.log(o);
+
+let num = 5;
+num = num+ 2;
+num+=2;
+num++;
+num--;
+++num;
+--num;
+console.log(num);
+
+// post increment
+let p = 5;
+let q = p++;
+console.log(p, q);
+ //pre increment
+
+let r = 5;
+let s = ++r;
+console.log(r, s);
+
+// power
+let t = 5;
+let u = 3;
+let v = t ** u;
+
+// using Math
+let w = Math.pow(5, 3);
+console.log(w);
