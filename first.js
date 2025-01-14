9aa504371cf26109a0bd6e63320fabaa0903b727
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-14 11:00:17 +0530

    console log, variables, constant

---
+++ first.js
@@ -0,0 +1,38 @@
+// Date: 2021-03-07
+// Creator: N.V.Kannangara
+
+// This is my first JavaScript program
+// This is a comment line
+
+// program to print Hello World
+console.log("Hello World");
+console.log(2+2);
+console.log(2*2);
+
+// variables 
+let num = 4;
+console.log(num);
+
+console.log(num+56);
+
+let user = "Nimal's Account";
+user = "Nuwan"
+console.log(user);
+
+// two variables cannot declare with the same name
+// let user = "Nuwan"; // error
+
+// constants
+const pi = 3.14159;
+console.log(pi);
+
+// find the radius of a circle
+let radius = 5;
+let area = pi * radius * radius;
+console.log(area);
+
+// cannot reassign a constant
+// pi = 9; // error
+
+
+
