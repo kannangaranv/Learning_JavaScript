49f2c2613b74c94e3b5b09c4efc775e9890e6e3b
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-14 11:15:35 +0530

    Data types

--- first.js
+++ first.js
@@ -34,5 +34,56 @@ console.log(area);
 // cannot reassign a constant
 // pi = 9; // error
 
+// data types
+let name = "Nuwan"; //string
+let age = 25; // number
+
+let num1 = 23355363645775
+console.log(num1*25);
+
+// floating point numbers
+let height = 5.9;
+console.log(height);
+
+// check the data type
+console.log(typeof name);
+
+// hexadecimal numbers
+let hex = 0xff;
+console.log(hex);
+
+// octal numbers
+let oct = 0o377;
+console.log(oct);
+
+// binary numbers
+let bin = 0b1101;
+console.log(bin);
+
+//exponential numbers
+let exp = 2e3;
+console.log(exp);
+
+// infinity
+let inf = -5/0
+console.log(inf);
+
+// maximum and minimum numbers
+let max = Number.MAX_VALUE;
+let min = Number.MIN_VALUE;
+console.log(max);
+console.log(min);
+
+//bigInt
+let big = 1234567890123456789012345678901234567890n;
+
+// add two big int numbers
+let big1 = 1234567890123456789012345678901234567890n;
+let big2 = 1234567890123456789012345678901234567890n;
+let sum = big1 + big2;
+console.log(sum);
+
+
+
 
 
