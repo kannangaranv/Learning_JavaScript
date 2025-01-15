887ee2e25423c1cb0a9a53d64f81f9ce47eae0a9
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-15 20:13:02 +0530

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,15 @@
+let num = 8;
+let result
+
+if (num % 2 === 0){
+    result = "Even";
+}else{
+    result = "Odd";
+}
+
+console.log(result);
+
+// Ternary operator
+
+let result2 = num % 2 === 0 ? "Even" : "Odd";
+console.log(result2);
