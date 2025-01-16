f1fd1d9e95ba1b58e8034fc20ba65db717ebb220
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-16 11:00:59 +0530

    Arrow functions

---
+++ Arrow_function.js
@@ -0,0 +1,13 @@
+let greet = (user) => {
+    return `Hello, ${user}!`;
+}
+
+
+let str = greet('Nuwan');
+console.log(str); // Hello World!
+
+let add = (a,b) => {
+    return Math.abs(a)+Math.abs(b);
+}
+
+console.log(add(5,6)); // 11
