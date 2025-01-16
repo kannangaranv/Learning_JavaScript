9691918af2e6c9377ecfc9011889f700d01b5541
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-16 10:05:41 +0530

    Objects

---
+++ object.js
@@ -0,0 +1,9 @@
+let alien = {
+    name: "Nuwan",
+    technology: "JavaScript",
+    'home planet': "Mars"
+}
+
+console.log(`Name of Alien ${alien.name}`); // object
+console.log(`Technology of Alien ${alien.technology}`); // object
+console.log(`Home Planet of Alien ${alien['home planet']}`); // object
