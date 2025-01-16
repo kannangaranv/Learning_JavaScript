63e9d0451cdd44c6dfe8bae097422ccd49acb1a8
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-16 17:04:22 +0530

    data types 2

--- dataType.js
+++ dataType.js
@@ -7,3 +7,21 @@ names[2].tech = "Python";
 console.log(names)
 names[3]();
 
+
+let data = [5,7,8,9,10];
+console.log(data);
+
+data.push(20);
+console.log(data);
+
+data.pop();
+console.log(data);
+
+data.shift();
+console.log(data);
+
+data.unshift(1);
+console.log(data);
+
+data.splice(2,1);
+console.log(data);
