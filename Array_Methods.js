38fa0707a1c0e9caa72862cef7a3a8273d03f97c
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-16 19:07:14 +0530

    Map, Filter, Reduce

--- Array_Methods.js
+++ Array_Methods.js
@@ -1,6 +1,8 @@
-let nums = [97,78,55,70,89,23]
+let nums = [97,78,55,70,89,24]
 console.log(nums);
 
-nums.forEach((n,i,nums)=>{
-    console.log(n*2,i,nums);
-})
+let result = nums.filter(n => n%2==0)
+    .map(n => n*2)
+    .reduce((a,b) => a+b);
+    
+console.log(result); // 296
